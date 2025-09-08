cf288cb3f099196312820314c4f8de17a595793c
laith <laithalawad745@gmail.com>
2025-09-09 02:35:02 +0300

    شسيشسيشسي

--- app/data/guessWhoData.js
+++ app/data/guessWhoData.js
@@ -3,38 +3,38 @@ export const guessWhoCharacters = [
   // الدور الأول - لاعبي ريال مدريد (char1 إلى char10)
   {
     id: 'char1',
-    name: 'كاما',
-    image: '../../cama.jpg'
-  },
-  {
-    id: 'char2',
-    name: 'إندريك',
-    image: '../../endrick.jpg'
-  },
-  {
-    id: 'char3',
-    name: 'رودريجو',
-    image: '../../rodrigojpg.jpg'
-  },
-  {
-    id: 'char4',
-    name: 'روديجر',
-    image: '../../rodeger.jpg'
-  },
-  {
-    id: 'char5',
-    name: 'مبابي',
-    image: '../../mbabe.jpg'
-  },
-  {
-    id: 'char6',
     name: 'فينيسيوس',
     image: '../../vini.jpg'
   },
   {
+    id: 'char2',
+    name: 'بنزيما',
+    image: '../../benzema.jpg'
+  },
+  {
+    id: 'char3',
+    name: 'كاماڤينغا',
+    image: '../../cama.jpg'
+  },
+  {
+    id: 'char4',
+    name: 'إندريك',
+    image: '../../endrick.jpg'
+  },
+  {
+    id: 'char5',
+    name: 'رودريغو',
+    image: '../../rodrigojpg.jpg'
+  },
+  {
+    id: 'char6',
+    name: 'رودريغر',
+    image: '../../rodeger.jpg'
+  },
+  {
     id: 'char7',
-    name: 'أرنولد',
-    image: '../../arnold.jpg'
+    name: 'إمبابي',
+    image: '../../mbabe.jpg'
   },
   {
     id: 'char8',
@@ -48,110 +48,162 @@ export const guessWhoCharacters = [
   },
   {
     id: 'char10',
-    name: 'بنزيما',
-    image: '../../benzema.jpg'
+    name: 'أرنولد',
+    image: '../../arnold.jpg'
   },
 
-  // الدور الثاني - الشخصيات الأولى السابقة (char11 إلى char20)
+  // الدور الثاني - الأعلام (char11 إلى char20)
   {
     id: 'char11',
+    name: 'تركيا',
+    image: '../../turkey.jpg'
+  },
+  {
+    id: 'char12',
+    name: 'الفلبين',
+    image: '../../philippines.jpg'
+  },
+  {
+    id: 'char13',
+    name: 'الصين',
+    image: '../../chaina.jpg'
+  },
+  {
+    id: 'char14',
+    name: 'المملكة المتحدة',
+    image: '../../unaitkingdom.jpg'
+  },
+  {
+    id: 'char15',
+    name: 'الأوروغواي',
+    image: '../../uruguay.jpg'
+  },
+  {
+    id: 'char16',
+    name: 'الهند',
+    image: '../../india.jpg'
+  },
+  {
+    id: 'char17',
+    name: 'اليابان',
+    image: '../../japan.jpg'
+  },
+  {
+    id: 'char18',
+    name: 'إيطاليا',
+    image: '../../italy.jpg'
+  },
+  {
+    id: 'char19',
+    name: 'فرنسا',
+    image: '../../france.jpg'
+  },
+  {
+    id: 'char20',
+    name: 'البرازيل',
+    image: '../../brazel.jpg'
+  },
+
+  // الدور الثالث - الشخصيات الأصلية الأولى (char21 إلى char30)
+  {
+    id: 'char21',
     name: 'الشاب خالد',
     image: '../../alshabkhaled.jpg'
   },
   {
-    id: 'char12',
+    id: 'char22',
     name: 'مريم',
     image: '../../merem.jpg'
   },
   {
-    id: 'char13',
+    id: 'char23',
     name: 'ميسي',
     image: '../../messi.jpg'
   },
   {
-    id: 'char14',
+    id: 'char24',
     name: 'رونالدو',
     image: '../../ronlado.jpg'
   },
   {
-    id: 'char15',
+    id: 'char25',
     name: 'رايا',
     image: '../../raya.jpg'
   },
   {
-    id: 'char16',
+    id: 'char26',
     name: 'شاكيرا',
     image: '../../shakera.jpg'
   },
   {
-    id: 'char17',
+    id: 'char27',
     name: 'نانسي',
     image: '../../nansi.jpg'
   },
   {
-    id: 'char18',
+    id: 'char28',
     name: 'عادل',
     image: '../../adel.jpg'
   },
   {
-    id: 'char19',
+    id: 'char29',
     name: 'احمد',
     image: '../../ahmad.jpg'
   },
   {
-    id: 'char20',
+    id: 'char30',
     name: 'هنيدي',
     image: '../../hnede.jpg'
   },
 
-  // الدور الثالث - الشخصيات الثانية السابقة (char21 إلى char30)
+  // الدور الرابع - الشخصيات الأصلية الثانية (char31 إلى char40)
   {
-    id: 'char21',
+    id: 'char31',
     name: 'دربحة',
     image: '../../darbha.jpg'
   },
   {
-    id: 'char22',
+    id: 'char32',
     name: 'عبسي',
     image: '../../fabsi.jpg'
   },
   {
-    id: 'char23',
+    id: 'char33',
     name: 'جورجينا',
     image: '../../georgina.jpg'
   },
   {
-    id: 'char24',
+    id: 'char34',
     name: 'ماهر',
     image: '../../mahercom.jpg'
   },
   {
-    id: 'char25',
+    id: 'char35',
     name: 'مستر بيست',
     image: '../../mrbest.jpg'
   },
   {
-    id: 'char26',
+    id: 'char36',
     name: 'صلاح',
     image: '../../salah.jpg'
   },
   {
-    id: 'char27',
+    id: 'char37',
     name: 'ترامب',
     image: '../../tramp.jpg'
   },
   {
-    id: 'char28',
+    id: 'char38',
     name: 'سبيد',
     image: '../../speed.jpg'
   },
   {
-    id: 'char29',
+    id: 'char39',
     name: 'ام عصام',
     image: '../../omesam.jpg'
   },
   {
-    id: 'char30',
+    id: 'char40',
     name: 'ابو فلة',
     image: '../../abofalah.jpg'
   }
@@ -165,7 +217,7 @@ export const getMatch1Characters = () => {
   });
 };
 
-// دالة للحصول على الشخصيات الأولى السابقة (char11-char20)
+// دالة للحصول على الأعلام (char11-char20)
 export const getMatch2Characters = () => {
   return guessWhoCharacters.filter(char => {
     const charNumber = parseInt(char.id.replace('char', ''));
@@ -173,10 +225,28 @@ export const getMatch2Characters = () => {
   });
 };
 
-// دالة للحصول على الشخصيات الثانية السابقة (char21-char30)
+// دالة للحصول على الشخصيات الأصلية الأولى (char21-char30)
 export const getMatch3Characters = () => {
   return guessWhoCharacters.filter(char => {
     const charNumber = parseInt(char.id.replace('char', ''));
     return charNumber >= 21 && charNumber <= 30;
   });
 };
+
+// دالة للحصول على الشخصيات الأصلية الثانية (char31-char40)
+export const getMatch4Characters = () => {
+  return guessWhoCharacters.filter(char => {
+    const charNumber = parseInt(char.id.replace('char', ''));
+    return charNumber >= 31 && charNumber <= 40;
+  });
+};
+
+// // دالة للحصول على لاعبي ريال مدريد فقط
+// export const getRealMadridPlayers = () => {
+//   return getMatch1Characters();
+// };
+
+// // دالة للحصول على الأعلام فقط
+// export const getCountryFlags = () => {
+//   return getMatch2Characters();
+// };
